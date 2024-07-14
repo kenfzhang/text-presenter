9f0ecfba758f413b2bdb720357e3ae9e5b034eb8
kenfzhang <4808683+kenfzhang@users.noreply.github.com>
2024-07-13 23:16:03 -0700

    initialize main loop

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-def main():
-    print("Hello, world!")
+import os
+from time import sleep
 
-if __name__ == '__main__':
-    main()
+FRAME_RATE = 60
+
+def render_screen(stuff):
+    for i in stuff:
+        line = "".join(i)
+        print(line)
+
+def create_screen(height, width):
+    screen = []
+    for i in range(0,height):
+        screen.append([])
+        for _ in range(0,width):
+            screen[i].append(".")
+    return screen
+
+def refresh_screen():
+    sleep(1/FRAME_RATE)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def main_loop():
+    screen = create_screen(10,20)
+    while True:
+        refresh_screen()
+        render_screen(screen)
+
+def main():
+    main_loop()
+
+main()
